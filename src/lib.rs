d31145dfe1057b0f62dee021007ea4d0b288294a
Bryan E. Barnhart <bryan.e.barnhart@gmail.com>
2016-11-06 20:13:47 -0700

    Added missing spaces between top level items, and missing rustdoc strings for functions and structs for release 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! This library is an implementation of the Blorb 2.0.4 specification.
+//! The specification can be found at the following web address:
+//!
+//! * http://www.eblong.com/zarf/blorb/blorb.html
+//!
+//! Blorbs are a resource file type used in *Interactive Fiction* (IF).
+//! They bundle together images, text, sounds, and other resources,
+//! along with executable code, for IF interpreters to use.
+//!
+//! This library gives access to the blorb file type and contents
+//! through structures. Additionally, it provides a lazy access
+//! interface to the blorb contents, allowing interpreters to use blorbs
+//! without dumping the full file contents contents into memory.
+//!
+//! **NOTE**: This library is not production ready. The interface is
+//! currently unstable, and only the lazy-loading portion of this
+//! library has been implemented.
+
 extern crate byteorder;
 
 mod blorb;
