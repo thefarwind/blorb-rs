e3e29701fa57236c27016dd4f2e60061a008c1a4
Bryan E. Barnhart <bryan.e.barnhart@gmail.com>
2016-11-16 23:23:41 -0700

    fixed type in Frontispiece identifier. Changed how identifier is displayed in docs. Added documentation for public struct memebers. Reworded some documentation to make it more accurate. Fixed documentation related to API change.

--- src/blorb.rs
+++ src/blorb.rs
@@ -3,15 +3,20 @@ use std::collections::HashMap;
 // Metadata Structs
 ////////////////////////////////////////////////////////////////////////
 
-/// Metadata struct for `FORM` chunks. Contains the length of the chunk
-/// and the form id of the chunk. Provides slightly more info into a
-/// `FORM` chunk so that use and contained data can be determined.
+/// Metadata struct for `FORM` chunks. Used for identifying a form
+/// without loading the full form into memory.
+///
+/// Forms are a type of a chunk which contain additional chunks. These
+/// are called out separately to chunks due to the large number of form
+/// types.
 ///
 /// **NOTE**: The `len` includes the 4 bytes in `id`. The remaining
 /// length of the chunk after the `id` is `len - 4`.
 #[derive(Debug)]
 pub struct FormData {
+    /// the length of the form, not counting the 8 byte chunk header
     pub len: u32,
+    /// the 4 byte ascii id. The length includes these bytes.
     pub id: [u8; 0x4],
 }
 
@@ -20,7 +25,9 @@ pub struct FormData {
 /// loading the full chunk into memory.
 #[derive(Debug)]
 pub struct ChunkData {
+    /// The 4 byte ascii id of the chunk
     pub id: [u8; 0x4],
+    /// The length of the form, not counting the 8 byte chunk header
     pub len: u32,
 }
 
@@ -39,19 +46,19 @@ impl From<FormData> for ChunkData {
 /// The usage information for an `IndexEntry`.
 #[derive(Debug)]
 pub enum Usage {
-    /// Identifier: 'Pict'.
+    /// Identifier: `b"Pict"`.
     /// Indicates the resource is an image.
     Pict,
 
-    /// Identifier: 'Snd '.
+    /// Identifier: `b"Snd "`.
     /// Indicates the resource is a sound.
     Snd,
 
-    /// Indentifier: 'Data'.
+    /// Indentifier: `b"Data"`.
     /// Indicates the resource is some data.
     Data,
 
-    /// Identifier: 'Exec'.
+    /// Identifier: `b"Exec"`.
     /// Indicates the resource is an executable.
     Exec,
 }
@@ -61,8 +68,11 @@ pub enum Usage {
 /// the entry, and where in the blob the entry starts.
 #[derive(Debug)]
 pub struct IndexEntry {
+    /// The type of the resource
     pub usage: Usage,
+    /// The index of the resource
     pub num: u32,
+    /// The starting address of the resource
     pub start: u32,
 }
 
@@ -70,6 +80,8 @@ pub struct IndexEntry {
 /// Container for list of resource index entries.
 #[derive(Debug)]
 pub struct ResourceIndex {
+    /// a map of index value of a resource to the index entry of the
+    /// resource.
     pub entries: HashMap<usize, IndexEntry>,
 }
 
@@ -82,31 +94,31 @@ pub enum Chunk {
     /// chunks, and this type will be used to do so when necessary.
     Unknown{meta: ChunkData, data: Vec<u8>},
 
-    /// Identifier: 'RIdx'.
+    /// Identifier: `b"RIdx"`.
     /// Contains a resource index for the IF.
-    /// This chunk is mandatory and must be the first chunk in the form.
+    /// This chunk is mandatory and must be the first chunk in the blorb.
     ResourceIndex{index: ResourceIndex},
 
-    /// Identifier: 'IFmd'.
+    /// Identifier: `b"IFmd"`.
     /// Contains xml metadata content for the IF.
     Metadata{info: String},
 
-    /// Identifier: 'Fspec'.
+    /// Identifier: `b"Fspc"`.
     /// Contains a reference to a frontispiece image.
     /// This chunk is optional.
     Frontispiece{num: u32},
 
-    /// Identifier: 'GLUL'.
+    /// Identifier: `b"GLUL"`.
     /// Contains Glulx executable.
     /// This is a executable resource chunk.
     Glulx{code: Vec<u8>},
 
-    /// Identifier: 'PNG '.
+    /// Identifier: `b"PNG "`.
     /// Contains a PNG image.
     /// This is a picture resource chunk.
     Png{data: Vec<u8>},
 
-    /// Identifier: 'JPEG'.
+    /// Identifier: `b"JPEG"`.
     /// Contains a JPEG image.
     /// This is a picture resource chunk.
     Jpeg{data: Vec<u8>},
